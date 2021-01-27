4490d761e249042c9cd19a7b2fe55e29b2e22302
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2021-01-27 10:10:46 +0000

    completing the arc

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ resource "azurerm_container_registry" "acr" {
   name                     = var.registryname
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  sku                      = "Premium"
-  admin_enabled            = false
+  sku                      = var.sku
+  admin_enabled            = var.admin_enabled
   georeplication_locations = var.georeplication_locations
+  network_rule_set         = var.network_rule_set
+  retention_policy         = var.retention_policy
 }
