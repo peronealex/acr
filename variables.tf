16b0e014d81093a8a7ffd06f3711b96926f1e81e
Alessio Perone <alessio.perone@lexisnexisrisk.com>
2021-01-27 09:51:06 +0000

    adding the example

--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "location" {
   default     = "westeurope"
   description = "location of the resource group"
 }
+
+variable "georeplication_locations" {
+  default = []
+  description = "list of the regions for replication ex ["East US", "West US"]"
+}
